4a4b75ec5ef89e26e95bc41d9d51632b4e2dd67e
ASUS <ASUS@WRCN>
2024-02-09 22:28:15 +0300

    Add project files.

---
+++ Workaround/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Business.Concrete;
+using Entities.Concrete;
+
+Person person = new Person();
+person.FirstName = "UĞUR CAN";
+person.LastName = "AKKOÇ";
+person.DateOfBirthYear = 1989;
+person.NationalIdentity = 11111;
+
+PttManager pttManager = new PttManager(new PersonManager());
+
+pttManager.GiveMask(person);
